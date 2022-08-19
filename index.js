794387a055686e4cb3cef3ab73ddfe5aa2c74d9f
spur237 <samuel.nzioka@student.moringaschool.com>
2022-08-19 17:33:12 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1=31
+const num2=2
+const multiply = num1 * num2;
+console.log(multiply)
+ const random =Math.floor(Math.random()*10)+1
+ console.log (random)
+ const num3=9
+ const num4=5
+ const mod= num3%num4
+ console.log (mod)
+ const max=Math.max(12,17,18,20,7)
+ console.log(max)
